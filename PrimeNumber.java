ac7556f542bbeaa8d76c8e4b4d65a1414f70c0d1
AllenDevaraj <86686418+AllenDevaraj@users.noreply.github.com>
2021-10-29 20:04:27 +0300

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class PrimeNumber{    
+	public static void main(String args[]){    
+		int i,m=0,pt=0;   
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please input a number");
+		int n = sc.nextInt();
+		m=n/2;      
+		if(n==0||n==1){  
+			System.out.println(n+" is not a prime number");      
+		}else{  
+			for(i=2;i<=m;i++){      
+				if(n%i==0){      
+					System.out.println(n+" is not a prime number");      
+					pt=1;      
+					break;      
+				}      
+			}      
+			if(pt==0)  { System.out.println(n+" is a prime number"); }  
+		} 
+	}    
+}   
